18f2244a4530273ac6f72b4aefe6ee87e68a5820
Evin Grano <etgryphon@cox.net>
2009-12-20 20:08:09 -0500

    added some terminal logic to verify link acceptance and added some crappy css to prove it...

--- apps/linkit-demo/models/human.js
+++ apps/linkit-demo/models/human.js
@@ -73,17 +73,48 @@ LinkItDemo.Human = SC.Record.extend(LinkIt.Node, {
   
   canLink: function(link) {
     if (!link) return NO;
-
+    
+    var sn = link.get('startNode'), st = link.get('startTerminal');
+    var en = link.get('endNode'), et = link.get('endTerminal');
+    
     // Make sure we don't connect to ourselves.
-    var endNode = link.get('endNode');
-    if (endNode === this) return NO;
+    if (sn === en) return NO;
+    //console.log('\nCan Link: (%@).%@ => (%@).%@'.fmt(SC.guidFor(sn), st, SC.guidFor(en), et));
 
     // Make sure we don't already have this link.
     if (this._hasLink(link)) return NO;
+    
+    var sGender = sn.get('isMale') ? 'boy' : 'girl';
+    var eGender = en.get('isMale') ? 'boy' : 'girl';
+    var terms = '%@:%@ %@:%@'.fmt(sGender, st, eGender, et);
+    if(sGender !== eGender && st === 'sig' && et === 'sig') { 
+      //console.log('Man Married To Woman'); 
+      return YES; 
+    }
 
-    return YES;
+    // Data Points
+    var dadHasKids = (terms.indexOf('boy:kids') > -1);
+    var hasDad = (terms.indexOf('dad') > -1);
+    var momHasKids = (terms.indexOf('girl:kids') > -1);
+    var hasMom = (terms.indexOf('mom') > -1);
+    
+    //console.log('dadKidsIdx: %@, hasDadIdx: %@, momKidsIdx: %@, hasMomIdx: %@'.fmt(dadHasKids, hasDad, momHasKids, hasMom));
+    
+    if(dadHasKids && hasDad) { 
+      //console.log('(%@,%@) Dad link to Kids: %@'.fmt(SC.guidFor(sn), SC.guidFor(en), terms )); 
+      return YES; 
+    }
+    
+    if(momHasKids && hasMom) { 
+      //console.log('(%@,%@) Mom link to Kids: %@'.fmt(SC.guidFor(sn), SC.guidFor(en), terms ));
+      return YES; 
+    }
+ 
+    // TODO: [EG] add the check to make sure there is no incest
+
+    return NO;
   },
-  
+    
   _hasLink: function(link) {
     var links = this.get('links') || [];
     var len = links.get('length'), n;
@@ -97,19 +128,30 @@ LinkItDemo.Human = SC.Record.extend(LinkIt.Node, {
   },
   
   didCreateLink: function(link) {
-    var sn = link.get('startNode');
-    var st = link.get('startTerminal');
-    var en = link.get('endNode');
-    var et = link.get('endTerminal');
-    console.log('(%@) SN: %@, ST: %@, EN, %@, ET: %@'.fmt(this.get('name'), sn.get('name'), st, en.get('name'), et));
+    var sn = link.get('startNode'), st = link.get('startTerminal');
+    var en = link.get('endNode'), et = link.get('endTerminal');
     if(et === 'sig'){
       this.set('spouse', en);
     }
-    else if (st === 'mom'){
-      this.set('mother', en);
+    else if (et === 'mom'){
+      this.set('mother', sn);
     }
-    else if (st === 'dad'){
-      this.set('father', en);
+    else if (et === 'dad'){
+      this.set('father', sn);
+    }
+  },
+  
+  willDeleteLink: function(link) {
+    var sn = link.get('startNode'), st = link.get('startTerminal');
+    var en = link.get('endNode'), et = link.get('endTerminal');
+    if(et === 'sig'){
+      this.set('spouse', null);
+    }
+    else if (et === 'mom'){
+      this.set('mother', null);
+    }
+    else if (et === 'dad'){
+      this.set('father', null);
     }
   }
 });
